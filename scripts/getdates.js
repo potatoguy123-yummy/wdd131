49cdee9b4d15207845b6245d49f86c9ab89d68db
Ethan O'Brien <ethan.a.obrienw@gmail.com>
2025-09-04 23:33:52 -0500

    week01

---
+++ scripts/getdates.js
@@ -0,0 +1,8 @@
+
+const date = new Date();
+
+const currentYear = date.getFullYear();
+
+document.getElementById("currentyear").innerText = currentYear;
+
+document.getElementById("lastModified").innerText = document.lastModified;
