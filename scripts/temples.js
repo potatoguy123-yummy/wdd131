4159bf5057bcb6ae19faad90683f3179197b3a17
Ethan O'Brien <ethan.a.obrienw@gmail.com>
2025-09-11 22:16:42 -0500

    temple assignmnet

---
+++ scripts/temples.js
@@ -0,0 +1,13 @@
+const date = new Date();
+const currentYear = date.getFullYear();
+document.getElementById("currentyear").innerText = currentYear;
+document.getElementById("lastModified").innerText = document.lastModified;
+
+const hamburgerContainer = document.querySelector(".hamburger-container");
+const nav = document.querySelector("nav");
+hamburgerContainer.addEventListener("click", (e) => {
+    const child = hamburgerContainer.firstElementChild;
+    child.classList.toggle("hamburger-button");
+    child.classList.toggle("x-button");
+    nav.classList.toggle("nav-hidden");
+});
